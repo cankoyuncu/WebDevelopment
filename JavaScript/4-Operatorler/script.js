dc861f853084b1cca390b84abdfb4aa6cb8ecb28
cankoyuncu <99793493+cankoyuncu@users.noreply.github.com>
2022-09-02 20:07:29 +0300

    Add files via upload

---
+++ JavaScript/4-Operatorler/script.js
@@ -0,0 +1,26 @@
+//matematiksel
+
+var x = 10;
+var y = 20;
+var z = 10;
+
+console.log(x+y);
+console.log(y/x);
+console.log(x*y+z);
+
+x++;
+x=x+1;
+console.log(x);
+
+//karşılaştırma
+
+var sonuc = x>y;
+console.log(sonuc);
+
+console.log(x<y);
+console.log(x>=z);
+console.log(x=="10");
+console.log(x!=z);
+
+//mantiksal operatorler
+
